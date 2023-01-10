55f493c427e165000d4096fda3ea005322be259a
Julián Lora <120677363+julianlora@users.noreply.github.com>
2023-01-10 20:13:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,22 @@ import Item from "./Item.js";
 import * as storage from "./storage.js";
 
 //CARGAR DATOS GUARDADOS
-if (storage.listOfLists !== null){ // Carga de listas
-  storage.listOfLists.forEach((element) => {
+if (storage.listOfLists() !== null){ // Carga de listas
+  storage.listOfLists().forEach((element) => {
     let list = new List(
       element.name,
-      0,
-      "grey",
-      false,
+      element.items,
+      element.progress,
+      element.status,
+      element.domName,
+      element.hideFinished
     );
     list.addList()
 
-    if (storage.listOfItems !== null){ // Carga de los items primarios para las listas
-      storage.listOfItems.forEach((e) => {
+    if (storage.listOfItems() !== null){ // Carga de los items primarios para las listas
+      storage.listOfItems().forEach((e) => {
         if (e.list === list.domName){
-          list.createItem(e.list, e.text, e.id, e.status)
+          list.createItem(e)
         }
       })
     }
@@ -28,13 +30,15 @@ if (storage.listOfLists !== null){ // Carga de listas
 
 const button = document.querySelector(".new-list-button")
 button.addEventListener("click", () =>{
-  if (document.querySelector(".new-list").value !== "" && (!storage.listOfLists.some( (e) => e.name === document.querySelector(".new-list").value))){
+  if (document.querySelector(".new-list").value !== "" && (!storage.listOfLists().some( (e) => e.name === document.querySelector(".new-list").value))){
     //CREATE LIST OBJECT (text, items, progress, status)
     let list = new List(
       document.querySelector(".new-list").value,
       0,
-      "grey",
-      false,
+      0,
+      'none',
+      document.querySelector(".new-list").value,
+      false
     );
 
     list.addList()
