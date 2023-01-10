b96d0e6f96e6d8166037bf87a1a60dba429e44d3
Julián Lora <120677363+julianlora@users.noreply.github.com>
2023-01-10 20:13:22 -0300

    Update List.js

--- List.js
+++ List.js
@@ -8,7 +8,8 @@ class List {
     items,
     progress,
     status,
-    domName
+    domName,
+    hideFinished,
   ) {
     // Define properties:
     this.name = name;
@@ -16,6 +17,7 @@ class List {
     this.progress = progress;
     this.status = status;
     this.domName = domName
+    this.hideFinished = hideFinished
   }
   // Add methods like normal functions
   addList(){
@@ -25,7 +27,7 @@ class List {
     if (isNaN(this.name)){
       this.domName = this.name.split(' ').join('')
     } else {
-      this.domName = "i" + this.name.split(' ').join('')
+      this.domName = "l" + this.name.split(' ').join('')
     }
 
     //ADD LIST TO DOM
@@ -35,14 +37,6 @@ class List {
       <h1 class="title-${this.domName}">
         ${this.name}
         <meter class="progress-${this.domName}"></meter>
-        <div class="dropdown">
-          <button class="dropbtn options-${this.domName}">Options</button>
-          <div id="myDropdown-${this.domName}" class="dropdown-content">
-            <button class="show-done-${this.domName}">Hide tasks done</button>
-            <a href="#about">About</a>
-            <a href="#contact">Contact</a>
-          </div>
-        </div>
         <button class="remove-${this.domName}">x</button>
       </h1>
       <ul class="list-${this.domName}"=></ul>
@@ -55,6 +49,78 @@ class List {
     newList.innerHTML = content
     document.querySelector("main").append(newList)
     document.querySelector(".new-list").value = ""
+
+    //SETUP ITEM 'ADD' BUTTON
+    const listButton = document.querySelector(`.add-${this.domName}`)
+    listButton.addEventListener("click", () => {
+      if (document.querySelector(`.input-${this.domName}`).value !== ""){
+        //ADD NEW ITEM
+        this.createItem()
+        document.querySelector(`.input-${this.domName}`).value = ""
+      }
+    }, false)
+    
+    //SETUP REMOVE LIST BUTTON
+    let removeButton = document.querySelector(`.remove-${this.domName}`)
+    removeButton.addEventListener("click", () => {
+      let lista = document.querySelector(`.article-${this.domName}`)
+      lista.remove()
+      storage.removeList(this.domName)
+    }, false)
+
+    this.optionsMenu(newList)
+
+    //SAVE IN ARRAY AND LOCAL STORAGE
+    console.log(this)
+    storage.saveList(this)
+  }
+
+  createItem(e) {
+    //CREATE ITEM
+    if (e == undefined){
+      const item = new Item(
+        Date.now(),
+        document.querySelector(`.input-${this.domName}`).value,
+        this.domName,
+        "uncomplete",
+        (new Date()).toDateString(),
+        0,
+        0,
+        this.domName
+      )
+      item.addItem()
+    } else {
+      const item = new Item(
+        e.id,
+        e.text,
+        e.list,
+        e.status,
+        e.creationDate,
+        e.subItems,
+        e.siblings,
+        e.mainList,
+      )
+      item.addItem()
+    }
+    
+  }
+
+  optionsMenu(listElement){
+
+    //add to DOM
+    let meter = document.querySelector(`.progress-${this.domName}`)
+    const optionsElement = document.createElement('div')
+    optionsElement.setAttribute('class', 'dropdown')
+    let content = `
+      <button class="dropbtn options-${this.domName}">Options</button>
+      <div id="myDropdown-${this.domName}" class="dropdown-content">
+        <button class="hidetasks-${this.domName} show">Hide finished tasks</button>
+        <button class="hideprogress-${this.domName} show">Hide progress</button>
+        <a href="#contact">Contact</a>
+      </div>
+    `
+    optionsElement.innerHTML = content
+    meter.insertAdjacentElement('afterend', optionsElement)
 
     //SETUP OPTIONS MENU
     /* When the user clicks on the button, 
@@ -78,38 +144,38 @@ class List {
       }
     }
 
-    //SETUP ITEM 'ADD' BUTTON
-    const listButton = document.querySelector(`.add-${this.domName}`)
-    listButton.addEventListener("click", () => {
-      if (document.querySelector(`.input-${this.domName}`).value !== ""){
-        //ADD NEW ITEM
-        this.createItem(this.domName)
-        document.querySelector(`.input-${this.domName}`).value = ""
+    // HIDE TASKS
+    const hideButton = document.querySelector(`.hidetasks-${this.domName}`)
+    hideButton.addEventListener("click", () => {
+      if (this.hideFinished){
+        this.hideFinished = false
+      } else {
+        this.hideFinished = true
       }
-    }, false)
-    
-    //SETUP REMOVE LIST BUTTON
-    let removeButton = document.querySelector(`.remove-${this.domName}`)
-    removeButton.addEventListener("click", () => {
-      let lista = document.querySelector(`.article-${this.domName}`)
-      lista.remove()
-      storage.removeList(this.domName)
-    }, false)
+      storage.updateList(this)
+      this.hideFinishedItems()
+    })
 
-    //SAVE IN ARRAY AND LOCAL STORAGE
-    storage.saveList(this)
+    // HIDE PROGRESS
+
   }
 
-  createItem(listName, itemText = "", itemId = "", itemStatus = "uncomplete") {
-    //CREATE ITEM
-    const item = new Item(
-      itemId == "" ? Date.now() : itemId,
-      itemText == "" ? document.querySelector(`.input-${this.domName}`).value : itemText,
-      listName != this.domName ? listName : this.domName,
-      itemStatus,
-      (new Date()).toDateString()      
-    )
-    item.addItem()
+  hideFinishedItems(){
+    const hideButton = document.querySelector(`.hidetasks-${this.domName}`)
+    let items = document.querySelectorAll(`.mainlist-${this.domName}.done`)
+      if (this.hideFinished){
+        items.forEach((e) => {
+          e.hidden = true
+        })
+        hideButton.innerText = "Show finished tasks"
+        hideButton.classList.remove("show")
+      } else {
+        items.forEach((e) => {
+          e.hidden = false
+        })
+        hideButton.classList.add("show")
+        hideButton.innerText = "Hide finished tasks"
+      }
   }
 }
 
