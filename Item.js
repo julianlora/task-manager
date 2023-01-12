c6a3b91c19ca637492e46e7952b95e9f0038fdc9
Julián Lora <120677363+julianlora@users.noreply.github.com>
2023-01-11 22:55:21 -0300

    Update Item.js

--- Item.js
+++ Item.js
@@ -10,7 +10,8 @@ class Item {
       creationDate,
       subItems,
       siblings,
-      mainList
+      mainList,
+      type
     ) {
       // Define properties:}
       this.id = id;
@@ -21,47 +22,166 @@ class Item {
       this.subItems = subItems;
       this.siblings = siblings;
       this.mainList = mainList;
+      this.type = type;
     }
     // Add methods like normal functions:
     
-    addItem(input = false) {
+    addItem(input = false, type = 'item') {
       // ADD ITEM TO DOM
       const element = document.createElement("li")
       element.classList.add(`item-${this.id}` , `parent-${this.list}`, `mainlist-${this.mainList}`)
-      
-      if (this.text === ""){
+
+      if (this.text == ""){
         this.text = document.querySelector(`.input-${this.list}`).value
       }
-      if (this.status == "uncomplete"){
-        let content = `
-          <input class="checkbox-${this.id} c${this.list}" type="checkbox">
-          ${this.text}
-          <button class="extend-${this.id}-${this.list}" hidden>v</button>
-          <button class="delete-${this.id}-${this.list}" hidden>x</button>
-        `
-        element.innerHTML = content
+      if (this.type == 'item'){
+        if (this.status == "uncomplete"){
+          let content = `
+            <input class="checkbox-${this.id} c${this.list}" type="checkbox">
+            ${this.text}
+            <button class="extend-${this.id}-${this.list}" hidden>v</button>
+            <button class="delete-${this.id}-${this.list}" hidden>x</button>
+          `
+          element.innerHTML = content
+        } else {
+          let content = `
+            <input class="checkbox-${this.id} c${this.list}" type="checkbox" checked>
+            ${this.text}
+            <button class="extend-${this.id}-${this.list}" hidden>v</button>
+            <button class="delete-${this.id}-${this.list}" hidden>x</button>
+          `
+          element.innerHTML = content
+          element.setAttribute("style", "text-decoration:line-through")
+          element.classList.add('done')
+        }
+        document.querySelector(`.list-${this.list}`).insertAdjacentElement("beforeend", element)
+        //SETUP CHECKBOX EVENT
+        this.createCheckbox()
       } else {
         let content = `
-          <input class="checkbox-${this.id} c${this.list}" type="checkbox" checked>
-          ${this.text}
+          <b>${this.text}</b>
           <button class="extend-${this.id}-${this.list}" hidden>v</button>
           <button class="delete-${this.id}-${this.list}" hidden>x</button>
         `
         element.innerHTML = content
-        element.setAttribute("style", "text-decoration:line-through")
-        element.classList.add('done')
+        element.classList.add('sublist')
+        document.querySelector(`.list-${this.list}`).insertAdjacentElement("beforeend", element)
       }
-      document.querySelector(`.list-${this.list}`).insertAdjacentElement("beforeend", element)
 
-      //SETUP CHECKBOX EVENT
+      //DELETE AND EXTEND BUTTON
+      this.createItemButtons(element)
+
+      //SAVE ITEM IN LOCAL STORAGE
+      storage.saveItem(this)
+      this.loadSubItems()
+      this.updateProgress()
+      if (input && this.type == 'item'){
+        this.checkboxEvent(false)
+      }
+    }
+
+    addSubItem(subItem, input = true){
+      if (input == true ? document.querySelector(`.input-${this.id}`).value !== "" : true){
+        let list = document.createElement('ul')
+        list.setAttribute("class", `list-${this.id}`)
+        if (input){
+          document.querySelector(`.extension-${this.id}`).insertAdjacentElement('beforebegin', list)
+          let newItem = new Item(
+            Date.now(), 
+            document.querySelector(`.input-${this.id}`).value, 
+            this.id, 
+            "uncomplete",
+            (new Date()).toDateString(),
+            0,
+            0, 
+            this.mainList,
+            'item')
+          newItem.addItem(input = true)
+          document.querySelector(`.input-${this.id}`).value = ""
+        } else {
+          document.querySelector(`.item-${this.id}.parent-${this.list}`).insertAdjacentElement('beforeend', list)
+          let newItem = new Item(
+            subItem.id, 
+            subItem.text,
+            this.id, 
+            subItem.status,
+            subItem.creationDate,
+            subItem.subItems, 
+            subItem.siblings, 
+            this.mainList,
+            subItem.type)
+          newItem.addItem()
+        }
+        
+      }
+    }
+
+    loadSubItems(){
+      storage.listOfItems().forEach((i) => {
+        if (this.id === i.list){
+          this.addSubItem(i, false)
+        }
+      })
+    }
+
+    createCheckbox(){
       let checkbox = document.querySelector(`.checkbox-${this.id}.c${this.list}`)
       checkbox.addEventListener("change", () => {
         this.checkboxEvent(checkbox.checked)
         this.updateProgress()
         storage.listOfLists().find((l) => l.domName == this.mainList).hideFinishedItems()
       })
+    }
 
-      //DELETE AND EXTEND BUTTON
+    checkboxEvent(checked){
+      this.subItems = Array.from(document.querySelectorAll(`.parent-${this.id}`)).length
+      this.siblings = Array.from(document.querySelectorAll(`.parent-${this.list}`)).length
+
+      let checkbox = document.querySelector(`.checkbox-${this.id}.c${this.list}`)
+      let element = document.querySelector(`.item-${this.id}.parent-${this.list}`)
+      let parent = storage.listOfItems().find((e) => e.id == this.list)
+      if (checked){ 
+        checkbox.checked = true
+        element.setAttribute("style", "text-decoration:line-through")
+        element.classList.add("done")
+        this.status = 'done'
+        storage.updateItem(this)
+        if (this.subItems != 0){
+          storage.listOfItems().forEach((i) => {
+            if (i.list == this.id && i.status == 'uncomplete'){
+              i.checkboxEvent(checked)
+            }
+          })
+        }
+        if (parent != undefined
+        && parent.type == 'item'
+        && parent.subItems == Array.from(document.querySelectorAll(`.c${this.list}`)).filter(i => i.checked).length
+        && parent.status == 'uncomplete'){
+          parent.checkboxEvent(checked)
+        }
+      } else {
+        checkbox.checked = false
+        element.removeAttribute("style")
+        element.classList.remove("done")
+        this.status = 'uncomplete'
+        storage.updateItem(this)
+        if (this.subItems != 0 && this.subItems == Array.from(document.querySelectorAll(`.c${this.id}`)).filter(i => i.checked).length){ // only if all subitems are done
+          storage.listOfItems().forEach((i) => {
+            if (i.list == this.id && i.status == 'done'){ 
+              i.checkboxEvent(checked)
+            }
+          })
+        }
+        if (parent != undefined
+          && parent.type == 'item'
+          && parent.status == 'done'){
+            parent.checkboxEvent(checked)
+          }
+      }
+      storage.updateItem(this)
+    }
+
+    createItemButtons(element){
       const deleteButton = document.querySelector(`.delete-${this.id}-${this.list}`)
       const extendButton = document.querySelector(`.extend-${this.id}-${this.list}`)
       element.addEventListener("mouseover", () => {
@@ -103,106 +223,14 @@ class Item {
           extendButton.classList.remove("opened")
         }
       })
-
-      //SAVE ITEM IN LOCAL STORAGE
-      storage.saveItem(this)
-      this.loadSubItems()
-      this.updateProgress()
-      if (input){
-        this.checkboxEvent(false)
-      }
-    }
-
-    addSubItem(subItem, input = true){
-      if (input == true ? document.querySelector(`.input-${this.id}`).value !== "" : true){
-        let list = document.createElement('ul')
-        list.setAttribute("class", `list-${this.id}`)
-        if (input){
-          document.querySelector(`.extension-${this.id}`).insertAdjacentElement('beforebegin', list)
-          let newItem = new Item(
-            Date.now(), 
-            document.querySelector(`.input-${this.id}`).value, 
-            this.id, 
-            "uncomplete",
-            (new Date()).toDateString(),
-            0,
-            0, 
-            this.mainList)
-          newItem.addItem(input = true)
-          document.querySelector(`.input-${this.id}`).value = ""
-        } else {
-          document.querySelector(`.item-${this.id}.parent-${this.list}`).insertAdjacentElement('beforeend', list)
-          let newItem = new Item(
-            subItem.id, 
-            subItem.text,
-            this.id, 
-            subItem.status,
-            subItem.creationDate,
-            subItem.subItems, 
-            subItem.siblings, 
-            this.mainList)
-          newItem.addItem()
-        }
-        
-      }
-    }
-
-    loadSubItems(){
-      storage.listOfItems().forEach((i) => {
-        if (this.id === i.list){
-          this.addSubItem(i, false)
-        }
-      })
-    }
-
-    checkboxEvent(checked){
-      this.subItems = Array.from(document.querySelectorAll(`.parent-${this.id}`)).length
-      this.siblings = Array.from(document.querySelectorAll(`.parent-${this.list}`)).length
-
-      let checkbox = document.querySelector(`.checkbox-${this.id}.c${this.list}`)
-      let element = document.querySelector(`.item-${this.id}.parent-${this.list}`)
-      let parent = storage.listOfItems().find((e) => e.id == this.list)
-      if (checked){ 
-        checkbox.checked = true
-        element.setAttribute("style", "text-decoration:line-through")
-        element.classList.add("done")
-        this.status = 'done'
-        storage.updateItem(this)
-        if (this.subItems != 0){
-          storage.listOfItems().forEach((i) => {
-            if (i.list == this.id && i.status == 'uncomplete'){
-              i.checkboxEvent(checked)
-            }
-          })
-        }
-        if (parent != undefined
-        && parent.subItems == Array.from(document.querySelectorAll(`.c${this.list}`)).filter(i => i.checked).length
-        && parent.status == 'uncomplete'){
-          parent.checkboxEvent(checked)
-        }
-      } else {
-        checkbox.checked = false
-        element.removeAttribute("style")
-        element.classList.remove("done")
-        this.status = 'uncomplete'
-        storage.updateItem(this)
-        if (this.subItems != 0 && this.subItems == Array.from(document.querySelectorAll(`.c${this.id}`)).filter(i => i.checked).length){ // only if all subitems are done
-          storage.listOfItems().forEach((i) => {
-            if (i.list == this.id && i.status == 'done'){ 
-              i.checkboxEvent(checked)
-            }
-          })
-        }
-        if (parent != undefined
-          && parent.status == 'done'){
-            parent.checkboxEvent(checked)
-          }
-      }
-      storage.updateItem(this)
     }
 
     updateProgress(){
 
+      // UPDATE ITEM DATA
+      storage.updateItem(this)
+
+      // UPDATE RELATED ITEMS DATA
       storage.listOfItems().forEach((i) => {
         if (i.mainList == this.mainList){
 
@@ -212,16 +240,17 @@ class Item {
         storage.updateItem(i)
       })
       
-      // UPDATE HIDDEN
+      // UPDATE ITEM VISIBILITY
       storage.listOfLists().find((l) => l.domName == this.mainList).hideFinishedItems()
       
+      //UPDATE PROGRESS BAR
       let progress = storage.listOfLists().find((e) => e.domName == this.list)
       if (progress != undefined){
         let total = 0, cant = 0
         storage.listOfItems().forEach((element) => {
-          if (element.list === this.list){
+          if (element.list == this.list && element.type != 'sublist'){
             total++
-            if (element.status === "done"){
+            if (element.status == "done"){
               cant++
             }
           }
@@ -233,6 +262,9 @@ class Item {
         bar.setAttribute("max", progress.items.toString())
         bar.innerText= `(${cant}/${progress.items})`
       }
+
+      // UPDATE NEW ITEM BUTTON POSITION
+      storage.listOfLists().find((l) => l.domName == this.mainList).newItemInput()
     }
 
     itemAge() {
