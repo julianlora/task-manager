842e7437b535757f239698b6eba33b8d33203e9b
Julián Lora <120677363+julianlora@users.noreply.github.com>
2023-01-14 01:53:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,42 @@ import List from "./List.js";
 import Item from "./Item.js";
 import * as storage from "./storage.js";
 
+//CSV HANDLER
+
+// save localStorage data in a csv file
+// (function(console){
+//   console.save = function(data, filename){
+//       if(!data) {
+//           console.error('Console.save: No data')
+//           return;
+//       }
+//       if(!filename) filename = 'console.json'
+//       if(typeof data === "object"){
+//           data = JSON.stringify(data, undefined, 4)
+//       }
+//       var blob = new Blob([data], {type: 'text/json'}),
+//           e    = document.createEvent('MouseEvents'),s
+//           a    = document.createElement('a')
+//       a.download = filename
+//       a.href = window.URL.createObjectURL(blob)
+//       a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
+//       e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+//       a.dispatchEvent(e)
+//   }
+// })(console)
+// console.save(JSON.parse(localStorage.getItem('items')), 'example-items.csv')
+
+if(JSON.parse(localStorage.getItem('lists')) == null){
+  let csvitems = await (await fetch('example-items.csv')).text();
+  let csvlists = await (await fetch('example-lists.csv')).text(); // assign csv data to a variable
+  if (csvitems[0] != '<' && csvlists[0] != '<'){
+    localStorage.setItem('items', csvitems)
+    localStorage.setItem('lists', csvlists)
+  } else {
+    console.log('example files not found')
+  }
+}
+
 //CARGAR DATOS GUARDADOS
 if (storage.listOfLists() !== null){ // Carga de listas
   storage.listOfLists().forEach((element) => {
